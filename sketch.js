fe97f630f5e9f97c0d1ad7a970f0554b3f9c8265
RayansatyaGupta <68857791+RayansatyaGupta@users.noreply.github.com>
2020-12-02 18:07:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+//Create variables here
+var dog;
+
+function preload(){
+  //load images here
+  
+}
+
+function setup() {
+	createCanvas(500, 500);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
